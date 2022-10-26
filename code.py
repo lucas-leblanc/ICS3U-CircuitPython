b95dc428c71f7f6ad0a79661a02c5cbe85ea3dc1
lucas-leblanc <113191554+lucas-leblanc@users.noreply.github.com>
2022-10-26 10:55:55 -0400

    added file

--- code.py
+++ code.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# Created by: Lucas LeBlanc
+# Created on: Oct 2022
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # his function is the main game game_scene 
+
+    print("\n\n\n") # 3 blank lines
+    Print("Hello, World!")
+
+    #repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
