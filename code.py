2d53de09ae01924d07fe704fa3b29d6fdb03a20e
lucas-leblanc <113191554+lucas-leblanc@users.noreply.github.com>
2022-10-31 11:05:19 -0400

    added file

--- code.py
+++ code.py
@@ -11,7 +11,7 @@ import stage
 def game_scene():
     # his function is the main game game_scene 
 
-#image banks for CircuitPython
+# image banks for CircuitPython
 image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
 # set the background to image 0 in the image bank
